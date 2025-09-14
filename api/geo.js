ad3dbc76510cea953e1cda9d2ffa4b013ae696a3
Maurox <xpresar@gmail.com>
2025-09-14 13:26:50 -0500

    Eliminar modifica geo js

--- api/geo.js
+++ api/geo.js
@@ -1,19 +1,13 @@
-export default async function handler(request) {
+export default async function handler(req, res) {
   try {
     const response = await fetch("http://ip-api.com/json/");
     const data = await response.json();
 
-    return new Response(JSON.stringify(data), {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*"
-      }
-    });
+    res.status(200).json(data);
   } catch (error) {
-    return new Response(
-      JSON.stringify({ error: "No se pudo obtener datos", details: error.message }),
-      { status: 500, headers: { "Content-Type": "application/json" } }
-    );
+    res.status(500).json({
+      error: "No se pudo obtener datos",
+      details: error.message
+    });
   }
 }
