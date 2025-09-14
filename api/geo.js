e4daaa3a76de909dde4dad2b6300180706b13edd
Maurox <xpresar@gmail.com>
2025-09-14 11:29:19 -0500

    Camcios 1

--- api/geo.js
+++ api/geo.js
@@ -1,13 +1,11 @@
-import fetch from "node-fetch";
-
 export default async function handler(req, res) {
   try {
-    const resp = await fetch("http://ip-api.com/json/"); // API original (solo HTTP)
-    const data = await resp.json();
+    const response = await fetch("http://ip-api.com/json/");
+    const data = await response.json();
 
-    res.setHeader("Access-Control-Allow-Origin", "*"); // habilita CORS
+    res.setHeader("Access-Control-Allow-Origin", "*"); // habilitar CORS
     res.status(200).json(data);
-  } catch (err) {
-    res.status(500).json({ error: "No se pudo obtener datos", details: err.message });
+  } catch (error) {
+    res.status(500).json({ error: "No se pudo obtener datos", details: error.message });
   }
 }
